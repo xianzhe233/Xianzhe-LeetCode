e4f584faad375c0a13befaf7e558dcc87656ed2b
xianzhe233 <xianzhe233@qq.com>
2025-03-14 10:24:21 +0800

    complete KMP 28

--- 28.find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.py
@@ -9,7 +9,41 @@
 class Solution:
 
     def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
+
+        def build_next(pattern):
+            next = [0]
+            L = 0
+            i = 1
+
+            while i < len(pattern):
+                if pattern[i] == pattern[L]:
+                    L += 1
+                    next.append(L)
+                    i += 1
+                else:
+                    if L == 0:
+                        next.append(L)
+                        i += 1
+                    else:
+                        L = next[L - 1]
+
+            return next
+
+        next = build_next(needle)
+        i, j = 0, 0
+
+        while i < len(haystack):
+            if haystack[i] == needle[j]:
+                i, j = i + 1, j + 1
+            elif j > 0:
+                j = next[j - 1]
+            else:
+                i += 1
+
+            if j == len(needle):
+                return i - j
+
+        return -1
 
 
 # @lc code=end
