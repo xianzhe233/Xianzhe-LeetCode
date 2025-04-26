7ed3f3eb0e4c2cd4cbd8f540a3aa3a147607edd3
xianzhe233 <xianzhe233@qq.com>
2025-04-26 14:56:28 +0800

    optimize 77

--- 77.combinations.py
+++ 77.combinations.py
@@ -11,30 +11,26 @@ class Solution:
 
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
+        cur_comb = []
 
-        def dfs(cur_comb, choices, rest=k):
+        def dfs(lower_bound, rest=k):
             """
-                `cur_comb`: generated combination till now
-                `choices`: numbers that can be chosen
+                `lower_bound`: smallest number that can be chosen, lower_bound <= choices <= n
                 `rest`: how many numbers can be added to comb
             """
-            nonlocal res
+            nonlocal res, cur_comb
 
             # base case
             if rest <= 0:
-                res.append(cur_comb)
+                res.append(cur_comb.copy())
                 return
 
-            # if we have no choice, we are fail to get a comb
-            if not choices:
-                return
+            for num in range(lower_bound, n + 1):
+                cur_comb.append(num)
+                dfs(num + 1, rest - 1)
+                cur_comb.pop()
 
-            for i, num in enumerate(choices):
-                if len(choices) - i < rest:
-                    break
-                dfs(cur_comb + [num], choices[i + 1:], rest - 1)
-
-        dfs([], list(range(1, n + 1)))
+        dfs(1)
 
         return res
 
