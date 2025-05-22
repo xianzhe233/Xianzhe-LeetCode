1d6f0925293aef02af271e521c6b95b6da345049
xianzhe233 <xianzhe233@qq.com>
2025-05-22 12:42:33 +0800

    complete 135

--- 135.candy.py
+++ 135.candy.py
@@ -10,78 +10,25 @@ from typing import List
 class Solution:
 
     def candy(self, ratings: List[int]) -> int:
+        candies_left = [1]
+        candies_right = [1]
 
-        def assign_candy(ratings):
-            n = len(ratings)
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i - 1]:
+                candies_left.append(candies_left[-1] + 1)
+            else:
+                candies_left.append(1)
 
-            # base case
-            if n <= 1:
-                return n
+        for i in range(len(ratings) - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies_right.append(candies_right[-1] + 1)
+            else:
+                candies_right.append(1)
 
-            # special case: first two ratings are identical
-            if ratings[0] == ratings[1]:
-                return 1 + self.candy(ratings[1:])
-
-            candies = [0]
-            min_candies = 0
-            recursive_ans = 0
-
-            def update_min():
-                nonlocal min_candies, candies
-                if candies[-1] == min(candies):
-                    min_candies = min(candies) - 1
-
-            for i in range(1, n):
-                pre_rating = ratings[i - 1]
-                cur_rating = ratings[i]
-
-                # special case: last child, no successive constraints
-                if i == n - 1:
-                    if pre_rating > cur_rating:
-                        # greedy
-                        update_min()
-                        candies.append(min_candies)
-                    else:
-                        candies.append(candies[-1] + 1)
-                    break
-
-                next_rating = ratings[i + 1]
-
-                # special case: adjacent identical ratings, recursively process rest part
-                if cur_rating == next_rating:
-                    if pre_rating > cur_rating:
-                        # greedy
-                        update_min()
-                        candies.append(min_candies)
-                    else:
-                        candies.append(candies[-1] + 1)
-                    recursive_ans = self.candy(ratings[i + 1:])
-                    break
-
-                # only remains strictly > or <
-                # 0 1 2
-                if pre_rating < cur_rating < next_rating:
-                    candies.append(candies[-1] + 1)
-                # 0 1 0
-                elif pre_rating < cur_rating > next_rating:
-                    candies.append(candies[-1] + 1)
-                # 1 0 1
-                elif pre_rating > cur_rating < next_rating:
-                    # greedy
-                    update_min()
-                    candies.append(min_candies)
-                # 2 1 0
-                else:
-                    candies.append(candies[-1] - 1)
-
-            # at least 1 candy for everyone
-            filler = 1 - min_candies
-            candies = list(map(lambda x: x + filler, candies))
-
-            return sum(candies) + recursive_ans
-
-        return min(assign_candy(ratings[:]), assign_candy(ratings[::-1]))
+        return sum([
+            max(left, right)
+            for left, right in zip(candies_left, candies_right[::-1])
+        ])
 
 
 # @lc code=end
-print(Solution().candy([1, 2, 87, 87, 87, 2, 1]))
