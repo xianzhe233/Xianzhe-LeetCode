e0ee5d6bd65dcf4316e13de87d440b637d4c305f
xianzhe233 <xianzhe233@qq.com>
2025-06-02 11:24:06 +0800

    746 optimized, O(1) space

--- 746.min-cost-climbing-stairs.py
+++ 746.min-cost-climbing-stairs.py
@@ -11,21 +11,20 @@ class Solution:
 
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         # top is 1 step after last index in `cost`
-        N = len(cost) + 1
-        INF = int(1e7)
+        N = len(cost)
 
         # min_cost[index]: min cost to reach `index`
         # at the beginning, min cost to every place is +infinity
-        min_cost = [INF] * N
         # can start from index 0 or 1
-        min_cost[0] = min_cost[1] = 0
+        min_cost = [0] * 3
 
-        for i in range(2, N):
+        for i in range(2, N + 1):
             # get here from 1 step behind or 2 steps behind
-            min_cost[i] = min(min_cost[i], min_cost[i - 1] + cost[i - 1],
-                              min_cost[i - 2] + cost[i - 2])
+            min_cost[2] = min(min_cost[2 - 1] + cost[i - 1],
+                              min_cost[2 - 2] + cost[i - 2])
+            min_cost[1], min_cost[0] = min_cost[2], min_cost[1]
 
-        return min_cost[-1]
+        return min_cost[2]
 
 
 # @lc code=end
